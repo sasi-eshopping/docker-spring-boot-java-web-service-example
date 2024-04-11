511d4ca482a71b6915ae08bc1708e5a0e646ca5e
sasi-eshopping <52425281+sasi-eshopping@users.noreply.github.com>
2024-04-11 17:38:22 +0530

    Update HelloController.java

--- src/main/java/com/levo/dockerexample/controller/HelloController.java
+++ src/main/java/com/levo/dockerexample/controller/HelloController.java
@@ -5,14 +5,42 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.*;
 @RestController
 @RequestMapping("docker-java-app")
 public class HelloController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test() {
-		return "docker-java-app is up and running: " + new Date();
+	
+	File file = new File(
+            "/data/testfile.txt");
+ 
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+ 
+        // Creating an object of BufferedReader class
+        BufferedReader br
+            = new BufferedReader(new FileReader(file));
+ 
+        // Declaring a string variable
+        String st;
+        StringBuilder sb1 = new 
+                      StringBuilder("This file content: "); 
+        System.out.println("Input: " + sb1); 
+  
+        // Appending the boolean value 
+        sb1.append(true); 
+
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null)
+	{
+            // Print the string
+            System.out.println(st);
+            sb1.append(String.valueOf(st));
+       }
+		return "docker-java-app is up and running: " + sb1;
 	}
 
 }
