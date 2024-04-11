23d2a749129664459adf2d348d17995225e2b287
sasi-eshopping <52425281+sasi-eshopping@users.noreply.github.com>
2024-04-11 18:10:20 +0530

    Update HelloController.java

--- src/main/java/com/levo/dockerexample/controller/HelloController.java
+++ src/main/java/com/levo/dockerexample/controller/HelloController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import java.io.*;
 @RestController
 @RequestMapping("docker-java-app")
